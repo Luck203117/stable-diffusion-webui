269df8dd0b765d323b7e9e893717221de77c9312
Luck203117 <mohammadrezanejati124@gmail.com>
2024-12-26 02:35:04 -0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\Ya-Mahdi\AppData\Local\Programs\Python\Python310
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set COMMANDLINE_ARGS= –xformers
+git pull
 call webui.bat
